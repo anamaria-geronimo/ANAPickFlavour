5611594e1129007d95d538e05f1d9fb453794938
Ana-Maria Stoica <stoica.anna91@gmail.com>
2018-10-17 12:02:43 +0300

    initial commit

---
+++ ANAPickFlavour/ANAPickFlavour.h
@@ -0,0 +1,19 @@
+//
+//  ANAPickFlavour.h
+//  ANAPickFlavour
+//
+//  Created by Ana-Maria Stoica on 17/10/2018.
+//  Copyright © 2018 Anamaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ANAPickFlavour.
+FOUNDATION_EXPORT double ANAPickFlavourVersionNumber;
+
+//! Project version string for ANAPickFlavour.
+FOUNDATION_EXPORT const unsigned char ANAPickFlavourVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANAPickFlavour/PublicHeader.h>
+
+
